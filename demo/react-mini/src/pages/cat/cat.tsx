3edc52919d909ddb97275faca40f978b77805fa0
Coooder-X <1104143015@qq.com>
2025-09-21 00:00:21 +0800

    refactor: 优化 demo，便于对比

--- demo/react-mini/src/pages/cat/cat.tsx
+++ demo/react-mini/src/pages/cat/cat.tsx
@@ -1,14 +1,16 @@
 import { View, Text } from '@tarojs/components';
 import { useLoad } from '@tarojs/taro';
 import * as d3 from 'd3';
+import { debounce } from 'lodash';
 import './index.scss';
 
-export default function Cat () {
+export default function Cat() {
   useLoad(() => {
     console.log('Page loaded.')
   })
 
   console.log(d3)
+  console.log(debounce)
 
   return (
     <View className='cat'>
--- demo/vue-mini/src/pages/cat/cat.vue
+++ demo/vue-mini/src/pages/cat/cat.vue
@@ -6,6 +6,7 @@
 
 <script setup lang="ts">
 import * as d3 from 'd3';
+import { debounce } from 'lodash';
 import { onMounted } from 'vue';
 import './index.scss';
 
@@ -14,6 +15,7 @@ onMounted(() => {
 });
 
 console.log(d3);
+console.log(debounce);
 </script>
 
 <style lang="scss">
